9c4f696daf6906e84a45c918eb0aaa7a060b8852
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-30 21:13:35 -0400

    Sample data

---
+++ seeds/scheduleData.js
@@ -0,0 +1,53 @@
+const { Schedule } = require('../models');
+
+const ScheduleData = [
+  {
+    id: 1,
+    first_name: 'John',
+    last_name: 'Doe',
+    days_working: 'Monday, Wednesday, Friday',
+    schedule_alteration: null,
+    pto_request: null,
+    call_in_sick_reason: null,
+  },
+  {
+    id: 2,
+    first_name: 'Jane',
+    last_name: 'Smith',
+    days_working: 'Tuesday, Thursday, Saturday',
+    schedule_alteration: 'Monday, Wednesday',
+    pto_request: '2023-04-05',
+    call_in_sick_reason: null,
+  },
+  {
+    id: 3,
+    first_name: 'Mike',
+    last_name: 'Johnson',
+    days_working: 'Monday - Friday',
+    schedule_alteration: null,
+    pto_request: '2023-05-01',
+    call_in_sick_reason: null,
+  },
+  {
+    id: 4,
+    first_name: 'Emily',
+    last_name: 'Davis',
+    days_working: 'Monday, Tuesday, Thursday, Friday',
+    schedule_alteration: 'Wednesday, Saturday',
+    pto_request: null,
+    call_in_sick_reason: 'Feeling unwell',
+  },
+  {
+    id: 5,
+    first_name: 'David',
+    last_name: 'Garcia',
+    days_working: 'Monday - Friday',
+    schedule_alteration: null,
+    pto_request: null,
+    call_in_sick_reason: null,
+  },
+];
+
+const seedScheduleData = () => ScheduleData.bulkCreate(ScheduleData);
+
+module.exports = seedScheduleData;
