ff5dc92c1a22d83a5cda0a1c46a777c572f56c7d
fardinatabassum <fardinatabassum@gmail.com>
2023-04-02 21:49:58 -0400

    added signup, login and logout handlebar, edited user.js and public files

---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in.');
+      }
+    }
+  };
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const firstName = document.querySelector('#firstname-signup').value.trim();
+    const lastName = document.querySelector('#lastname-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ firstName, lastName, username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
