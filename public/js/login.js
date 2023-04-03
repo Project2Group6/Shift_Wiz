e7d4e9a162dfb1f795f3124992474c09803c1845
fardinatabassum <fardinatabassum@gmail.com>
2023-04-03 19:14:19 -0400

    fixed variables for login and signup

--- public/js/login.js
+++ public/js/login.js
@@ -22,8 +22,7 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const firstName = document.querySelector('#firstname-signup').value.trim();
-    const lastName = document.querySelector('#lastname-signup').value.trim();
+
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
@@ -31,7 +30,7 @@ const loginFormHandler = async (event) => {
     if (firstName && lastName && username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ firstName, lastName, username, email, password }),
+        body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -47,7 +46,5 @@ const loginFormHandler = async (event) => {
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+
   
