48b7689466319441a85f8d2ea4979c2998711338
fardinatabassum <fardinatabassum@gmail.com>
2023-04-03 20:17:39 -0400

    commented out redundant user.js and fixed variables in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -23,11 +23,10 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
 
-    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (firstName && lastName && username && email && password) {
+    if (email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
