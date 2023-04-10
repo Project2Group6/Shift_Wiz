f9d4fb5f0ab8bbc69df10b2ffb46fd6e475f318e
Parth0415 <parthmali1597@gmail.com>
2023-04-10 17:03:42 -0400

    Made changes in handlebars

---
+++ public/js/pto.js
@@ -0,0 +1,18 @@
+
+var ptoInput = document.getElementById('ptoInput');
+  var todaysDate = new Date(); // Get today's date.
+
+  // Get today's date.
+
+  var year = todaysDate.getFullYear(); // yyyy
+
+  var month = ("0" + (todaysDate.getMonth() + 1)).slice(-2); // mm
+
+  var day = ("0" + todaysDate.getDate()).slice(-2); // dd
+
+  var dtToday = year + "-" + month + "-" + day; // Results in yyyy-mm-dd
+
+  // Now set the max date value for the calendar to be that date.
+
+  //$(".Past input").attr("max", dtToday);
+ptoInput.setAttribute('min', dtToday);
