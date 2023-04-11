6b9188f6a393fb23020b8aefe35642cd687e1168
DomenicoBarb <domenicobarberi585@gmail.com>
2023-04-11 14:39:05 -0400

    added fardina's modals/css

--- public/js/pto.js
+++ public/js/pto.js
@@ -1,20 +1,47 @@
 const ptoHandler = async (event) => {
-    event.preventDefault();
-  
-    const ptoStartDate = document.querySelector('#pto-start-date').value;
-    const ptoEndDate = document.querySelector('#pto-end-date').value;
-    // const ptoReason = document.querySelector('#pto-reason').value;
-  
-      const response = await fetch('/api/request/pto', {
-        method: 'POST',
-        body: JSON.stringify({ptoStartDate, ptoEndDate, type: 'pto'
-        }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
+  event.preventDefault();
 
-    // save button class here
-  document
-    .querySelector('.pto-form')
-    // Need to add re-direct to a different page after submitting
-    .addEventListener('submit', ptoHandler);
+  const ptoStartDate = document.querySelector('#pto-start-date').value;
+  const ptoEndDate = document.querySelector('#pto-end-date').value;
+  // const ptoReason = document.querySelector('#pto-reason').value;
+  // // prompting and closing the modal
+  const modal = document.getElementById("pto-modal");
+  const emptyModal = document.getElementById("empty-modal");
+  const ptoSaveBtn = document.getElementById("save-pto");
+  const modalClose = document.querySelector("#pto-modal-close");
+  const emptyModalClose = document.querySelector("#empty-modal-close");
+
+  modal.style.display = "none";
+  emptyModal.style.display = "none";
+
+  modalClose.addEventListener("click", function () {
+    modal.style.display = "none";
+    document.location.href = "/schedule";
+  });
+
+  emptyModalClose.addEventListener("click", function () {
+    emptyModal.style.display = "none";
+  });
+
+  if (ptoStartDate === "" && ptoEndDate === "") {
+    emptyModal.style.display = "block";
+    // Open the modal
+    // ...
+  } else {
+    // Show an error message or do nothing
+    const response = await fetch('/api/request/pto', {
+      method: 'POST',
+      body: JSON.stringify({
+        ptoStartDate, ptoEndDate, type: 'pto'
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    modal.style.display = "block";
+  }
+}
+
+// save button class here
+document
+  .querySelector('.pto-form')
+  // Need to add re-direct to a different page after submitting
+  .addEventListener('submit', ptoHandler);
--- public/js/sickcall.js
+++ public/js/sickcall.js
@@ -1,19 +1,51 @@
 const timeOffHandler = async (event) => {
-    event.preventDefault();
-  
-    const sickDate = document.querySelector('#sickCallDate').value;
-    const sickReason = document.querySelector('#sickCallReason').value.trim();
-  
-      const response = await fetch('/api/request/sick-calls', {
-        method: 'POST',
-        body: JSON.stringify({sickDate, type : "sick", sickReason,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
+  event.preventDefault();
 
-    // save button class here
-  document
-    .querySelector('.sick-call-form')
-    // Need to add re-direct to a different page after submitting
-    .addEventListener('submit', timeOffHandler);
+  const sickDate = document.querySelector('#sickCallDate').value;
+  const sickReason = document.querySelector('#sickCallReason').value;
+
+  // // prompting and closing the modal
+  const modal = document.getElementById("sick-modal");
+  const emptyModal = document.getElementById("empty-modal");
+  const sickSaveBtn = document.getElementById("save-sick-call");
+  const modalClose = document.querySelector("#sick-modal-close");
+  const emptyModalClose = document.querySelector("#empty-modal-close");
+
+  modal.style.display = "none";
+  emptyModal.style.display = "none";
+
+  modalClose.addEventListener("click", function () {
+    modal.style.display = "none";
+    document.location.href = "/schedule";
+  });
+
+  emptyModalClose.addEventListener("click", function () {
+    emptyModal.style.display = "none";
+  });
+
+  if (sickReason === "" && sickDate === "") {
+    emptyModal.style.display = "block";
+    // Open the modal
+    // ...
+  } else {
+    // Show an error message or do nothing
+    // ...
+    const response = await fetch('/api/request/sick-calls', {
+      method: 'POST',
+      body: JSON.stringify({
+        sickDate, type: "sick", sickReason,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    modal.style.display = "block";
+  }
+
+
+}
+
+// save button class here
+document
+  .querySelector('.sick-call-form')
+  // Need to add re-direct to a different page after submitting
+  .addEventListener('submit', timeOffHandler);
