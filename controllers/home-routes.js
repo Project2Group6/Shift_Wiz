954eb65c48c1a712881c7e2afd9069936137a6cd
DomenicoBarb <domenicobarberi585@gmail.com>
2023-04-11 17:23:08 -0400

    up to date code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,8 @@ const { Employee, TimeOff, User } = require('../models');
 const withAuth = require('../utils/auth');
 const getSched = require('../public/js/renderSched');
 const { raw } = require('express');
+const dayjs = require('dayjs')
+const today = dayjs()
 
 router.get('/', async (req, res) => {
   try {
@@ -24,8 +26,57 @@ router.get('/', async (req, res) => {
 // GET schedule page (TEMPLATE CODE)
 router.get('/schedule', withAuth, async (req, res) => {
   try {
-    const employeeData = await getSched;
-    // TODO: Fetch schedule data from the database and pass it to the template
+
+      // schedule shows 7 days, current day is first
+      const week = []
+      const weekDays = []
+      for (i = 0; i < 7; i++) {
+          week.push(today.add(i, 'day').day())
+          if(week[i] === 0) {
+              weekDays.push('Sun')
+          } else if (week[i] === 1) {
+              weekDays.push('Mon')
+          } else if (week[i] === 2) {
+              weekDays.push('Tue')
+          } else if (week[i] === 3) {
+              weekDays.push('Wed')
+          } else if (week[i] === 4) {
+              weekDays.push('Thu')
+          } else if (week[i] === 5) {
+              weekDays.push('Fri')
+          } else if (week[i] === 6) {
+              weekDays.push('Sat')
+          }
+      }  
+
+      const weekDates = []
+      var dayDatePairs = []
+      const fullDates = []
+      for (i = 0; i < 7; i ++) {
+          weekDates.push(today.add(i, 'day').format('DD'))
+          fullDates.push(today.add(i, 'day').format('YYYY-MM-DD'))
+      }
+      for(i = 0; i < 7; i++) {
+          dayDatePairs.push({day: weekDays[i], weekDate: weekDates[i], fullDate: fullDates[i]})
+      };
+        // Fetch schedule data from the database
+          const schedData = await Employee.findAll({
+                      attributes: ['id', 'first_name', 'last_name', 
+                      'works_sunday', 'works_monday','works_tuesday','works_wednesday',
+                      'works_thursday','works_friday','works_saturday',] 
+          })
+
+          const timeOffData = await TimeOff.findAll({
+              attributes: ['id', 'start_date', 'end_date', 'type', 'call_in_sick_reason', 'employee_id'],
+              include: {
+                  model: Employee,
+                  attributes: ['first_name', 'last_name', 'id'],
+                  as: 'employee',
+              }
+          })
+          const employeeData = {sched: schedData.map((id) => 
+          id.get({plain: true})), days: dayDatePairs, timeOff: timeOffData.map((id) => id.get({plain: true}))}
+    // Fetch schedule data from the database and pass it to the template
     res.render('schedule', {
       sched: employeeData.sched,
       days: employeeData.days,
@@ -40,7 +91,6 @@ router.get('/schedule', withAuth, async (req, res) => {
 
 // GET profile page (TEMPLATE CODE)
 router.get('/profile', withAuth, async (req, res) => {
-  // TEMP!! ↓ ------------------------------------------------------------------------------ ↓
   try {
     const userData = await User.findByPk(req.session.userId, {
       attributes: ['username', 'email'],
@@ -55,8 +105,6 @@ router.get('/profile', withAuth, async (req, res) => {
     if (!userData) {
       throw new Error('User data not found');
     }
-    // const user = userData.get({ plain: true });
-    // TEMP!! ↑ ------------------------------------------------------------------------------ ↑
     // render profile handlebar and pass user specific data
     res.render('profile', {
       profile: userData.get({plain:true}),
@@ -91,7 +139,6 @@ router.get('/availability', withAuth, async (req, res) => {
 
 // GET sick calls page (TEMPLATE CODE)
 router.get('/sick-calls', withAuth, async (req, res) => {
-  // TEMP!! ↓ -------------------------------------------------------------------------------- ↓
   try {
     res.render('sickCall', {
       loggedIn: req.session.loggedIn,
@@ -104,7 +151,6 @@ router.get('/sick-calls', withAuth, async (req, res) => {
 
 // GET PTO page (TEMPLATE CODE)
 router.get('/pto', withAuth, async (req, res) => {
-  // TEMP!! ↓ ---------------------------------------------------------------------------------- ↓
   try {
     res.render('pto', {
       loggedIn: req.session.loggedIn,
@@ -131,4 +177,3 @@ router.get('/signup', (req, res) => {
 });
 
 module.exports = router;
-
