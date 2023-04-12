e031d597ee9ee2cd6c61effaf76f48f06d674d59
Parth0415 <parthmali1597@gmail.com>
2023-04-11 21:51:46 -0400

    Final touches

--- public/js/pto.js
+++ public/js/pto.js
@@ -40,6 +40,41 @@ const ptoHandler = async (event) => {
   }
 }
 
+
+
+var ptoInput = document.getElementById('pto-start-date');
+var ptoEndDate = document.getElementById('pto-end-date');
+  var todaysDate = new Date(); // Get today's date.
+
+  // Get today's date.
+
+  var year = todaysDate.getFullYear(); // yyyy
+
+  var month = ("0" + (todaysDate.getMonth() + 1)).slice(-2); // mm
+
+  var day = ("0" + todaysDate.getDate()).slice(-2); // dd
+
+  var dtToday = year + "-" + month + "-" + day; // Results in yyyy-mm-dd
+
+  // Now set the max date value for the calendar to be that date.
+
+  //$(".Past input").attr("max", dtToday);
+ptoInput.setAttribute('min', dtToday);
+
+console.log("hello" + ptoInput.value);
+
+if(!ptoInput.value){
+  ptoEndDate.disabled = true
+}
+ptoInput.onchange = function (){
+  console.log(ptoInput.value);
+  ptoEndDate.disabled = false;
+
+  ptoEndDate.setAttribute('min', ptoInput.value);
+}
+
+
+
 // save button class here
 document
   .querySelector('.pto-form')
